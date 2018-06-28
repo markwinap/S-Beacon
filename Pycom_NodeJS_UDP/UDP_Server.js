e0308c4ba925f5749da2b54467b014b76b131334
Marco David Martinez <markwinap@gmail.com>
2018-06-27 19:31:58 -0500

    UDP Server Updated

--- Pycom_NodeJS_UDP/UDP_Server.js
+++ Pycom_NodeJS_UDP/UDP_Server.js
@@ -2,7 +2,6 @@
 UDP
 Beacon - {"pycom":"30aea4500ec4","rssi":-42,"beacon":"c30b6529c756a18a","beacon_time":59410,"battery":3,"dBm":6,"distance":0.56,"updated":1529331262857}
 Beacon Config {"beacon":"c6091790a9118b17","created":1529211600000,"updated":1529330940665,"alarm":false,"alarm_time":1529330655363,"alarm_reason":false,"alarms_triggered":8,"muted":false,"threshold":60000,"zones":["30aea4500ec4"],"in_ok_zones":[{"pycom":"30aea4500ec4","rssi":-65,"beacon":"c6091790a9118b17","beacon_time":512,"battery":3.01,"dBm":4,"distance":10}],"in_notok_zones":[]}
-
 */
 
 const dgram = require('dgram');
@@ -14,13 +13,13 @@ const bodyParser = require('body-parser');
 const udp_server = dgram.createSocket('udp4');
 const express_app = express();
 express_app.use(bodyParser.json());
-const salt = new Buffer('Maxpayne32', 'utf8');
+const salt = new Buffer('SALT', 'utf8');
 
 client = redis.createClient();
 multi = client.multi();
 
 //Timmers
-const min_update = 20000;
+const min_update = 30000;
 const interval_update = 20000;
 
 //Server Ports
@@ -31,14 +30,18 @@ const udpPort = 33333;
 //Data for testing - Clear - Set
 client.hdel('beacon_config', 'c30b6529c756a18a');
 client.hdel('beacon_config', 'c6091790a9118b17');
-client.hset('beacon_config', 'c30b6529c756a18a', JSON.stringify({beacon: 'c30b6529c756a18a', created:  1529211600000, updated: 1529211600000, alarm: false, alarm_time: 1529287200000, alarm_reason: false, alarms_triggered: 0, muted: false, threshold: 60000, in_ok_zones: [], in_notok_zones: [], zones: ['30aea4500ec4']}));
-client.hset('beacon_config', 'c6091790a9118b17', JSON.stringify({beacon: 'c6091790a9118b17', created:  1529211600000, updated: 1529211600000, alarm: false, alarm_time: 1529287200000, alarm_reason: false, alarms_triggered: 0, muted: false, threshold: 60000, in_ok_zones: [], in_notok_zones: [], zones: ['30aea4500ec4']}));
-
-//c30b6529c756a18a: '{"beacon":"c30b6529c756a18a","created":1529211600000,"updated":1529211600000,"alarm":false,"alarm_time":1529287200000,"alarm_reason":false,"alarms_triggered":0,"muted":false,"threshold":60000,"in_ok_zones":[],"in_notok_zones":[],"zones":["30aea4500ec4"]}' }
+client.hset('beacon_config', 'c30b6529c756a18a', JSON.stringify({beacon: 'c30b6529c756a18a', created:  1529211600000, updated: 1529211600000, alarm: false, inzone_time: 0, alarm_time: 1529287200000, alarm_reason: false, alarms_triggered: 0, muted: false, threshold: 60000, in_ok_zones: [], in_notok_zones: [], zones: ['30aea4500ec4']}));
+client.hset('beacon_config', 'c6091790a9118b17', JSON.stringify({beacon: 'c6091790a9118b17', created:  1529211600000, updated: 1529211600000, alarm: false, inzone_time: 0, alarm_time: 1529287200000, alarm_reason: false, alarms_triggered: 0, muted: false, threshold: 60000, in_ok_zones: [], in_notok_zones: [], zones: ['30aea4500ec4']}));
 
 
 let update_interval = setInterval(() => updateDB(), interval_update);
- 
+
+express_app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "*");
+	  next();
+});
+
 express_app.get('/getall', function (req, res) {
 	console.log('GET Request - query');
 	client.hgetall('beacon_config', function (err, beacon_c) {
@@ -106,6 +109,7 @@ udp_server.on('message', (msg, rinfo) => {
 				distance : distance(dBm, rssi),
 				updated: new Date().getTime()
 			};
+			//console.log(json);//DEBUG sds
 			multi.hset("beacon", json.beacon, JSON.stringify(json));////Hash Set
 			multi.sadd("pycom_id", json.pycom);//Set Add
 			multi.sadd("beacon_id", json.beacon);
@@ -155,7 +159,7 @@ function retHash(buf){
 	return hash.digest();
 }
 //update functions
-function compareBeacon(beacon, beacon_config, beacon_status){
+function compareBeacon(beacon, beacon_config, beacon_status){//Beacon ID, All beacon Configurations, ALL Beacons
 	let time = new Date().getTime();
 	let beacon_s_arr = Object.keys(beacon_status);
 	let in_ok_zones = [];
@@ -164,7 +168,7 @@ function compareBeacon(beacon, beacon_config, beacon_status){
 	for(let n in beacon_s_arr){
 		let b_status = JSON.parse(beacon_status[beacon_s_arr[n]]);
 		if(b_status.beacon == beacon){
-			if((time - b_status.updated) < min_update){
+			if((time - b_status.updated) < min_update){//Dont show old tokens
 				if(b_obj.zones.includes(b_status.pycom)) in_ok_zones.push(b_status);
 				else in_notok_zones.push(b_status);
 			}
@@ -180,9 +184,8 @@ function updateDB(){
 				let time = new Date().getTime();
 				if(err) console.log(err);
 				else{
-					console.log(beacon_s)
-					if(beacon_s){
-					
+					//console.log(beacon_s)
+					if(beacon_s){					
 						let beacon_c_obj = Object.keys(beacon_c);
 						for(let i in beacon_c_obj){
 							let beacon_obj = JSON.parse(beacon_c[beacon_c_obj[i]]);
@@ -190,38 +193,59 @@ function updateDB(){
 							if(beacon.in_ok_zones.concat(beacon.in_notok_zones).length != 0){
 								if(beacon.in_ok_zones.length > 0){
 									console.log(beacon_obj.beacon +  ' In ok Zone');
+									if(beacon_obj.alarm == true){
+										postAxios({Message: 'Beacon' + beacon_obj.beacon + '  Back Online', Subject: 'Beacon' + beacon_obj.beacon + ' Back Online'});
+
+									}
 									beacon_obj.alarm = false;
 									beacon_obj.alarm_reason = false;
 									beacon_obj.updated = time;
+									beacon_obj.inzone_time = time;
 								}
 								else{
-									if((time - beacon_obj.alarm_time) > beacon_obj.threshold){
-										console.log(beacon_obj.beacon +  ' In not ok Zone and > threshold' + (time - beacon_obj.alarm_time));
-										beacon_obj.updated = time;
-										beacon_obj.alarm = true;
-										beacon_obj.alarm_reason = 2;
-										if(!beacon_obj.muted) beacon_obj.alarm_time = time, beacon_obj.alarms_triggered ++;
+									if((time - beacon_obj.inzone_time) > beacon_obj.threshold){
+										
+										if(beacon_obj.alarm ==  false && beacon_obj.muted == false){
+											console.log(beacon_obj.beacon +  ' In not ok Zone and > threshold' + (time - beacon_obj.alarm_time));
+											beacon_obj.alarm = true;
+											beacon_obj.alarm_reason = 2;
+											beacon_obj.alarm_time = time;
+											beacon_obj.updated = time;
+											beacon_obj.alarms_triggered ++;
+											postAxios({Message: 'Beacon' + beacon_obj.beacon + '  Not Allowed Zone.\nAlarms Triggered ' +  beacon_obj.alarms_triggered, Subject: 'Beacon' + beacon_obj.beacon + ' Not Allowed Zone'});
+										}
+										else{
+											beacon_obj.alarm = true;
+											beacon_obj.alarm_reason = 2;
+										}
 									}
 									else{
 										console.log(beacon_obj.beacon +  ' In not ok Zone and < threshold');
-										beacon_obj.alarm = true;
+										beacon_obj.alarm = false;
 										beacon_obj.updated = time;
 										beacon_obj.alarm_reason = 2;
 									}
 								}
 							}
 							else{
-								if((time - beacon_obj.alarm_time) > beacon_obj.threshold){
-									console.log(beacon_obj.beacon +  ' Invisible > threshold ' + (time - beacon_obj.alarm_time));
-									beacon_obj.updated = time;
-									beacon_obj.alarm = true;
-									beacon_obj.alarm_reason = 1;
-									if(!beacon_obj.muted) beacon_obj.alarm_time = time, beacon_obj.alarms_triggered ++;
+								if((time - beacon_obj.updated) > beacon_obj.threshold){									
+									if(beacon_obj.alarm ==  false && beacon_obj.muted == false){
+										console.log(beacon_obj.beacon +  ' Invisible > threshold ' + (time - beacon_obj.updated));
+										beacon_obj.alarm = true;
+										beacon_obj.alarm_reason = 1;
+										beacon_obj.alarm_time = time;
+										beacon_obj.alarms_triggered ++;
+										postAxios({Message: 'Beacon' + beacon_obj.beacon + ' Not Visible.\nAlarms Triggered ' +  beacon_obj.alarms_triggered, Subject: 'Beacon' + beacon_obj.beacon + ' Not Visible'});
+									}
+									else{
+										beacon_obj.alarm = true;
+										beacon_obj.alarm_reason = 1;
+										//muted
+									}
 								}
 								else{
 									console.log(beacon_obj.beacon +  ' Invisible < threshold');
-									beacon_obj.alarm = true;
-									beacon_obj.updated = time;
+									beacon_obj.alarm = false;
 									beacon_obj.alarm_reason = 1;
 								}
 							}//Else No Zones
@@ -229,10 +253,33 @@ function updateDB(){
 							multi.hset('beacon_config', beacon_obj.beacon, JSON.stringify(Object.assign(beacon_obj, beacon)));
 							beacon_obj = {};
 						}//End For Loop
+						//Add New Beacons
+						for(let b in beacon_s){
+							let b_obj = JSON.parse(beacon_s[b]);
+							let dat = new Date().getTime();
+							if(!beacon_c_obj.includes(b_obj.beacon)){
+								multi.hset('beacon_config', b_obj.beacon, JSON.stringify({beacon: b_obj.beacon, created:  dat, updated: dat, alarm: false, inzone_time: 0, alarm_time: 0, alarm_reason: false, alarms_triggered: 0, muted: false, threshold: 60000, in_ok_zones: [], in_notok_zones: [], zones: ['30aea4500ec4']}));
+							}
+						}
 						multi.exec();
-				}			
-				}//Else If data		
+					}//IF Beacon Response is empty
+				}//Else If data
 			});//End Get beacon status
 		}//End Else
 	});//End beacon config
+}
+//POST AXIOS
+function postAxios(data){
+	axios({
+		method: 'post',
+		url: 'https://qmfrgy8wx6.execute-api.us-east-1.amazonaws.com/dev/s-beacon-sns-notification',
+		headers: {'Content-Type': 'application/json'},
+		data: JSON.stringify(data)
+	  })
+	  .then((response) => {
+		console.log(response)
+	  })
+	  .catch((error) => {
+		console.log(error);
+	  });	
 }
